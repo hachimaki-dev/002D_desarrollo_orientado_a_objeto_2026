e120596fbd5c6cffc3ed9d214931c6220e75795a
Hachimaki-dev <hachimakidev@gmail.com>
2026-08-11 17:05:30 -0400

    Mis apuntes de la clase del dia de hoy. Aprendimos a crear clases con atributos, comportamientos, instancias de clase y ademas buenas practicas de desarrollo y a chamullar mucho

---
+++ src/Perro.java
@@ -0,0 +1,11 @@
+public class Perro {
+   String nombre;
+   String raza;
+   int edad;
+   double peso;
+   boolean vacuna;
+
+   public String ladrar(){
+      return "Guauuu guauu!";
+   }
+}
---
+++ src/PerroSinPoo.java
@@ -0,0 +1,28 @@
+public class PerroSinPoo {
+   public static void main(String[] args) {
+        String nombre = "Cachupin";
+        String raza = "Quiltro";
+        int edad = 4;
+        double peso = 22.3;
+        boolean vacuna = true;
+
+        System.out.println("====== Bienvenido al sistema de Peritos ======");
+        System.out.println("Nombre del Perro");
+        System.out.println(nombre);
+
+        System.out.println("Edad del Perro");
+        System.out.println(edad);
+
+        System.out.println("Raza del Perro");
+        System.out.println(raza);
+
+        System.out.println("Peso del Perro");
+        System.out.println(peso);
+
+        System.out.println("Tiene vacuna?");
+        System.out.println(vacuna);
+
+        System.out.println("====== Gracias por acceder, los quiero mucho ======");
+
+   }
+}
