1fb76b623baf8620bc1dc7ad9bf78aee682eca00
ElD10SPAPU <danielopazo18.04@gmail.com>
2026-09-24 18:36:17 -0300

    Confirmo mi entrega de mi evaluacion

--- src/Taller.java
+++ src/Taller.java
@@ -5,6 +5,7 @@ public class Taller {
     static ArrayList<VehiculoParticular> coleccionvehiculosparticular = new ArrayList<>();
     static ArrayList<VehiculoCarga> coleccionvehiculoscarga = new ArrayList<>();
     static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
         MostrarMenu();
     }
@@ -46,7 +47,8 @@ public class Taller {
             }
         }
     }
-    static void RegistrarVehiculo(){
+
+    static void RegistrarVehiculo() {
         boolean bucle_menu = true;
         System.out.println(("--- Tipo de vehiculo ---"));
         while (bucle_menu) {
@@ -68,78 +70,138 @@ public class Taller {
                 default:
                     System.out.println("ERROR. Ingrese una opción del 1 al 3");
                     break;
-                }
             }
         }
-    
-    static void RegistrarVehiculoParticular(){
-        System.out.println("Patente: ");
-        String patente = sc.nextLine();
-        System.out.println("Marca: ");
-        String marca = sc.nextLine();
-        System.out.println("Revisiones pendientes: ");
-        int revisionesPendientes = Integer.parseInt(sc.nextLine());
-        System.out.println("Número de pasajeros: ");
-        int numPasajeros = Integer.parseInt(sc.nextLine());
-        VehiculoParticular VehiculoParticular = new VehiculoParticular(patente, marca, revisionesPendientes, numPasajeros);
-        coleccionvehiculosparticular.add(VehiculoParticular);
     }
 
-    static void RegistrarVehiculoCarga(){
-        System.out.println("Patente: ");
-        String patente = sc.nextLine();
-        System.out.println("Marca: ");
-        String marca = sc.nextLine();
-        System.out.println("Revisiones pendientes: ");
-        int revisionesPendientes = Integer.parseInt(sc.nextLine());
-        System.out.println("Peso maximo de carga (kg): ");
-        int pesoMaxCarga = Integer.parseInt(sc.nextLine());
-        VehiculoCarga VehiculoCarga = new VehiculoCarga(patente, marca, revisionesPendientes, pesoMaxCarga);
-        coleccionvehiculoscarga.add(VehiculoCarga);
-    }
-
-    static void ListarVehiculos(){
-        for (VehiculoCarga vehiculoCarga : coleccionvehiculoscarga) {
-            System.out.println(vehiculoCarga.mostrarInfo());
+    static void RegistrarVehiculoParticular() {
+        try {
+            // PATENTE
+            System.out.println("Patente: ");
+            String patente = sc.nextLine();
+            // MARCA
+            System.out.println("Marca: ");
+            String marca = sc.nextLine();
+            // REVISIONES PENDIENTES
+            System.out.println("Revisiones pendientes: ");
+            int revisionesPendientes = Integer.parseInt(sc.nextLine());
+            // NUMERO DE PASAJEROS
+            System.out.println("Número de pasajeros: ");
+            int numPasajeros = Integer.parseInt(sc.nextLine());
+            // AGREGAR EL VEHICULO PARTICULAR A SU COLECCIÓN
+            VehiculoParticular vehiculoParticular = new VehiculoParticular(patente, marca, revisionesPendientes,
+                    numPasajeros);
+            coleccionvehiculosparticular.add(vehiculoParticular);
+        } catch (NumberFormatException e) {
+            System.out.println("ERROR. Ingrese un número entero positivo mayor a 0");
         }
-        for (VehiculoParticular vehiculoParticular : coleccionvehiculosparticular) {
-            System.out.println(vehiculoParticular.mostrarInfo());
-        }
+
     }
 
-    static void BuscarVehiculoPorPatente(){
-        System.out.println("Ingrese la patente del vehiculo: ");
-        String patete_a_buscar = sc.nextLine();
-        for (VehiculoCarga vehiculoCarga : coleccionvehiculoscarga) {
-            if (vehiculoCarga.getPatente().contains(patete_a_buscar)){
+    static void RegistrarVehiculoCarga() {
+        try {
+            // PATENTE
+            System.out.println("Patente: ");
+            String patente = sc.nextLine();
+            // MARCA
+            System.out.println("Marca: ");
+            String marca = sc.nextLine();
+            // REVISIONES PENDIENTES
+            System.out.println("Revisiones pendientes: ");
+            int revisionesPendientes = Integer.parseInt(sc.nextLine());
+            // PESO MAXIMO DE CARGA
+            System.out.println("Peso maximo de carga (kg): ");
+            int pesoMaxCarga = Integer.parseInt(sc.nextLine());
+            // AGREGAR EL VEHICULO CARGA A SU COLECCIÓN
+            VehiculoCarga VehiculoCarga = new VehiculoCarga(patente, marca, revisionesPendientes, pesoMaxCarga);
+            coleccionvehiculoscarga.add(VehiculoCarga);
+        } catch (NumberFormatException e) {
+            System.out.println("ERROR. Ingrese un número enero positivo mayor a 0");
+        }
+
+    }
+
+    static void ListarVehiculos() {
+        if ((coleccionvehiculoscarga.isEmpty() && coleccionvehiculosparticular.isEmpty()) == true) {
+            System.out.println(
+                    "ERROR. Nuestra base de datos se encuentra vacía, ingrese datos con la 'OPCIÓN 1' para poder listarlos.");
+        } else {
+            for (VehiculoCarga vehiculoCarga : coleccionvehiculoscarga) {
                 System.out.println(vehiculoCarga.mostrarInfo());
             }
-        }
-        for (VehiculoParticular vehiculoParticular : coleccionvehiculosparticular) {
-            if (vehiculoParticular.getPatente().contains(patete_a_buscar)){
+            for (VehiculoParticular vehiculoParticular : coleccionvehiculosparticular) {
                 System.out.println(vehiculoParticular.mostrarInfo());
             }
         }
     }
 
-    static void RealizarRevision(){
-        
+    static void BuscarVehiculoPorPatente() {
+        if ((coleccionvehiculoscarga.isEmpty() && coleccionvehiculosparticular.isEmpty()) == true) {
+            System.out.println(
+                    "ERROR. Nuestra base de datos se encuentra vacía, ingrese datos con la 'OPCIÓN 1' para poder listarlos.");
+        } else {
+            System.out.println("Ingrese la patente del vehiculo: ");
+            String patete_a_buscar = sc.nextLine();
+            for (VehiculoCarga vehiculoCarga : coleccionvehiculoscarga) {
+                if (vehiculoCarga.getPatente().contains(patete_a_buscar)) {
+                    System.out.println(vehiculoCarga.mostrarInfo());
+                }
+            }
+            for (VehiculoParticular vehiculoParticular : coleccionvehiculosparticular) {
+                if (vehiculoParticular.getPatente().contains(patete_a_buscar)) {
+                    System.out.println(vehiculoParticular.mostrarInfo());
+                }
+            }
+        }
     }
 
-    static void ResumenDeVehiculos(){
-        int total_vehiculos = (coleccionvehiculosparticular.size() + coleccionvehiculoscarga.size());
-        int total_vehiculos_particulares = coleccionvehiculosparticular.size();
-        int total_vehiculos_de_carga = coleccionvehiculoscarga.size();
-        int ingresos_proyectados = 0;
+    static void RealizarRevision() {
+        int contador = 1;
         for (VehiculoCarga vehiculoCarga : coleccionvehiculoscarga) {
-            ingresos_proyectados += vehiculoCarga.calcularCostoRevision();
+            System.out.println("[" + contador + "]" + vehiculoCarga.getPatente() + " | " + vehiculoCarga.getMarca());
+            contador += 1;
         }
         for (VehiculoParticular vehiculoParticular : coleccionvehiculosparticular) {
-            ingresos_proyectados += vehiculoParticular.calcularCostoRevision();
+            System.out.println("[" + contador + "]" + vehiculoParticular.getPatente() + " | " + vehiculoParticular.getMarca());
+            contador += 1;
         }
-        System.out.println("Total vehiculos: " + total_vehiculos);
-        System.out.println("Vehiculos particulares: " + total_vehiculos_particulares);
-        System.out.println("Vehiculos de carga: " + total_vehiculos_de_carga);
-        System.out.println("Ingresos proyectados: " + ingresos_proyectados);
+        System.out.println("Ingrese la patente del vehiculo al que desea realizarle la revisión: ");
+        String patete_a_buscar = sc.nextLine();
+            for (VehiculoCarga vehiculoCarga : coleccionvehiculoscarga) {
+                if (vehiculoCarga.getPatente().contains(patete_a_buscar)) {
+                    int revisionesPendientes = vehiculoCarga.getRevisionesPendientes();
+                    revisionesPendientes -= 1;
+                    vehiculoCarga.setRevisionesPendientes(revisionesPendientes);
+                }
+            }
+            for (VehiculoParticular vehiculoParticular : coleccionvehiculosparticular) {
+                if (vehiculoParticular.getPatente().contains(patete_a_buscar)) {
+                    int revisionesPendientes = vehiculoParticular.getRevisionesPendientes();
+                    revisionesPendientes -= 1;
+                    vehiculoParticular.setRevisionesPendientes(revisionesPendientes);
+                }
+            }
+    }
+
+    static void ResumenDeVehiculos() {
+        if ((coleccionvehiculoscarga.isEmpty() && coleccionvehiculosparticular.isEmpty()) == true) {
+            System.out.println(
+                    "ERROR. Nuestra base de datos se encuentra vacía, ingrese datos con la 'OPCIÓN 1' para poder listarlos.");
+        } else {
+            int total_vehiculos = (coleccionvehiculosparticular.size() + coleccionvehiculoscarga.size());
+            int total_vehiculos_particulares = coleccionvehiculosparticular.size();
+            int total_vehiculos_de_carga = coleccionvehiculoscarga.size();
+            int ingresos_proyectados = 0;
+            for (VehiculoCarga vehiculoCarga : coleccionvehiculoscarga) {
+                ingresos_proyectados += vehiculoCarga.calcularCostoRevision();
+            }
+            for (VehiculoParticular vehiculoParticular : coleccionvehiculosparticular) {
+                ingresos_proyectados += vehiculoParticular.calcularCostoRevision();
+            }
+            System.out.println("Total vehiculos: " + total_vehiculos);
+            System.out.println("Vehiculos particulares: " + total_vehiculos_particulares);
+            System.out.println("Vehiculos de carga: " + total_vehiculos_de_carga);
+            System.out.println("Ingresos proyectados: " + ingresos_proyectados);
+        }
     }
 }
