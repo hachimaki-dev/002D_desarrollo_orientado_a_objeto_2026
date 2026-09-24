42b8dc975c98f5c4241da728293e1b9a75c4620c
seba1304256 <seba13email.com@gmail.com>
2026-09-24 18:40:31 -0300

    Rogelio Huichacura - confirmo entrega

--- src/Taller.java
+++ src/Taller.java
@@ -6,12 +6,14 @@ public class Taller {
     static ArrayList<VehiculoParticular> coleccion_VehivulosParticular = new ArrayList<>();
     static ArrayList<VehiculoCarga> coleccion_VehiculoCarga = new ArrayList<>();
     static Scanner sc = new Scanner(System.in);
+    static int contadorRevisiones = 0;
 
     public static void main(String[] args) {
-        
+
         mostrarMenu();
     }
-    static void mostrarMenu(){
+
+    static void mostrarMenu() {
         boolean mostrarMenu = true;
         while (mostrarMenu) {
             System.out.println("=====TALLER MECANICO=====");
@@ -28,38 +30,40 @@ public class Taller {
             switch (opcionDelMenu) {
                 case "1":
                     RegistrarVehiculos();
-                    
+
                     break;
                 case "2":
                     ListarVehiculo();
-                    
+
                     break;
                 case "3":
                     buscarVehiculo();
-                    
+
                     break;
                 case "4":
                     realizarRevision();
-                    
+
                     break;
                 case "5":
-                    
+                    resumenVehiculo();
+
                     break;
                 case "6":
                     mostrarMenu = false;
-                    
+
                     break;
                 case "7":
                     datosdeprueba();
-                    
+
                     break;
-            
+
                 default:
                     break;
             }
         }
     }
-    static void RegistrarVehiculos(){
+
+    static void RegistrarVehiculos() {
         boolean opcionSubMenu = true;
         while (opcionSubMenu) {
             System.out.println("====TIPO DE VEHICULO===");
@@ -78,23 +82,23 @@ public class Taller {
                     try {
                         System.out.println("revisiones pendientes : ");
                         int revisiones = Integer.parseInt(sc.nextLine());
-                        if (revisiones < 0 ) {
-                            System.out.println("no puede ser numeros negativos");
-                            return; 
-                        }
-                        System.out.println("numero de pasajeros:");
-                        int numerosPasajeros = Integer.parseInt(sc.nextLine());
-                        if (numerosPasajeros< 0) {
+                        if (revisiones < 0) {
                             System.out.println("no puede ser numeros negativos");
                             return;
                         }
-                        coleccion_VehivulosParticular.add(new VehiculoParticular(patente, marca, revisiones, numerosPasajeros));
-
+                        System.out.println("numero de pasajeros:");
+                        int numerosPasajeros = Integer.parseInt(sc.nextLine());
+                        if (numerosPasajeros < 0) {
+                            System.out.println("no puede ser numeros negativos");
+                            return;
+                        }
+                        coleccion_VehivulosParticular
+                                .add(new VehiculoParticular(patente, marca, revisiones, numerosPasajeros));
 
                     } catch (NumberFormatException e) {
                         System.out.println("ingrese solo numeros");
                     }
-                    
+
                     break;
                 case "2":
                     System.out.println("=====VEHICULO CARGA===");
@@ -105,50 +109,52 @@ public class Taller {
                     try {
                         System.out.println("revisiones pendientes : ");
                         int revisiones = Integer.parseInt(sc.nextLine());
-                        if (revisiones <= 0 ) {
+                        if (revisiones <= 0) {
                             System.out.println("no puede ser numeros negativos");
-                            return; 
+                            return;
                         }
                         System.out.println("numero de carga maxima en kilogramos ::");
                         int Carga = Integer.parseInt(sc.nextLine());
-                        if (Carga<= 0) {
+                        if (Carga <= 0) {
                             System.out.println("no puede ser numeros negativos");
                             return;
                         }
                         coleccion_VehiculoCarga.add(new VehiculoCarga(patentes, marcas, revisiones, Carga));
 
-
                     } catch (NumberFormatException e) {
                         System.out.println("ingrese solo numeros");
                     }
-                    
+
                     break;
                 case "3":
                     opcionSubMenu = false;
                     break;
-            
+
                 default:
                     break;
             }
         }
     }
-    static void ListarVehiculo(){
+
+    static void ListarVehiculo() {
         System.out.println("====INVENTARIO=====");
         System.out.println("vehiculos particular");
         for (VehiculoParticular vehiculoParticular : coleccion_VehivulosParticular) {
-            System.out.println(vehiculoParticular.mostrarInfo());            
+            System.out.println(vehiculoParticular.mostrarInfo());
         }
         System.out.println("vehiculo de Carga");
         for (VehiculoCarga vehiculoCarga : coleccion_VehiculoCarga) {
             System.out.println(vehiculoCarga.mostrarInfo());
         }
     }
-    static void datosdeprueba(){
+
+    static void datosdeprueba() {
         coleccion_VehivulosParticular.add(new VehiculoParticular("DR2023", "TOYOTA", 6, 4));
-         coleccion_VehivulosParticular.add(new VehiculoParticular("DR3451", "SUZUKI", 3, 4));
+        coleccion_VehivulosParticular.add(new VehiculoParticular("DR3451", "SUZUKI", 3, 4));
         coleccion_VehiculoCarga.add(new VehiculoCarga("FG3456", "VOLVO", 10, 5000));
     }
-    static void buscarVehiculo(){
+
+    static void buscarVehiculo() {
         System.out.println("ingrese patente del vehiculo");
         String patenteaBuscar = sc.nextLine().toUpperCase();
         for (VehiculoCarga vehiculoCarga : coleccion_VehiculoCarga) {
@@ -156,7 +162,6 @@ public class Taller {
                 System.out.println(vehiculoCarga.mostrarInfo());
             }
 
-            
         }
         for (VehiculoParticular vehiculoParticular : coleccion_VehivulosParticular) {
             if (vehiculoParticular.getpatente().contains(patenteaBuscar)) {
@@ -164,8 +169,9 @@ public class Taller {
             }
         }
     }
-    static void realizarRevision(){
-        if (coleccion_VehivulosParticular.isEmpty()|| coleccion_VehiculoCarga.isEmpty()) {
+
+    static void realizarRevision() {
+        if (coleccion_VehivulosParticular.isEmpty() || coleccion_VehiculoCarga.isEmpty()) {
             System.out.println("INVENTARIO VACIO");
 
         }
@@ -180,22 +186,109 @@ public class Taller {
             switch (opcionrevisar) {
                 case "1":
                     for (int i = 0; i < coleccion_VehivulosParticular.size(); i++) {
-                        VehiculoParticular v = coleccion_VehivulosParticular.get(i);
+                        VehiculoParticular vehiculo = coleccion_VehivulosParticular.get(i);
+                        System.out.println((i + 1) + "." + " patente del vehiculo: " + vehiculo.getpatente()
+                                + " marca del vehiculo :" + vehiculo.getmarca() + ". revisiones pendientes :"
+                                + vehiculo.getrevisionesPendientes());
+
                     }
+                    try {
+                        System.out.println("ingrese un numero");
+                        int revisionRealizar = Integer.parseInt(sc.nextLine());
+                        if (revisionRealizar < 1 || revisionRealizar > coleccion_VehivulosParticular.size()) {
+                            System.out.println(
+                                    "el numero igresado es menor a 0 o el vehiculo seleccionada no esta en la lista");
+                            return;
+
+                        }
+                        VehiculoParticular vehiculoSeleccionado = coleccion_VehivulosParticular
+                                .get(revisionRealizar - 1);
+                        System.out.println("ingrese la cantidad de revisiones realizadas :");
+                        int cantidadRevisiones = Integer.parseInt(sc.nextLine());
+                        if (cantidadRevisiones <= 0) {
+                            System.out.println("la cantidad no debe ser menor o igual a 0");
+                            return;
+                        }
+                        if (cantidadRevisiones > vehiculoSeleccionado.getrevisionesPendientes()) {
+                            System.out.println("la cantidad ingresada es mayor a la revisiones pendientes");
+                            return;
+                        }
+                        vehiculoSeleccionado.settrevisionesPendientes(
+                                vehiculoSeleccionado.getrevisionesPendientes() - cantidadRevisiones);
+                        contadorRevisiones++;
+                        System.out.println("revission realizada");
+                        System.out.println(vehiculoSeleccionado.mostrarInfo());
+
+                    } catch (NumberFormatException e) {
+                        System.out.println("tienes que ingreser solo numeros porfa");
+                    }
+
                     break;
                 case "2":
-                    
+                    for (int i = 0; i < coleccion_VehiculoCarga.size(); i++) {
+                        VehiculoCarga vehiculo = coleccion_VehiculoCarga.get(i);
+                        System.out.println((i + 1) + "." + " patente del vehiculo: " + vehiculo.getpatente()
+                                + " marca del vehiculo :" + vehiculo.getmarca() + ". revisiones pendientes :"
+                                + vehiculo.getrevisionesPendientes());
+
+                    }
+                    try {
+                        System.out.println("ingrese un numero");
+                        int revisionRealizar = Integer.parseInt(sc.nextLine());
+                        if (revisionRealizar < 1 || revisionRealizar > coleccion_VehiculoCarga.size()) {
+                            System.out.println(
+                                    "el numero igresado es menor a 0 o el vehiculo seleccionado no esta en la lista");
+                            return;
+
+                        }
+                        VehiculoCarga vehiculoSeleccionado = coleccion_VehiculoCarga.get(revisionRealizar - 1);
+                        System.out.println("ingrese la cantidad de revisiones realizadas :");
+                        int cantidadRevisiones = Integer.parseInt(sc.nextLine());
+                        if (cantidadRevisiones <= 0) {
+                            System.out.println("la cantidad no debe ser menor o igual a 0");
+                            return;
+                        }
+                        if (cantidadRevisiones > vehiculoSeleccionado.getrevisionesPendientes()) {
+                            System.out.println("la cantidad ingresada es mayor a la revisiones pendientes");
+                            return;
+                        }
+                        vehiculoSeleccionado.settrevisionesPendientes(
+                                vehiculoSeleccionado.getrevisionesPendientes() - cantidadRevisiones);
+                        contadorRevisiones++;
+                        System.out.println("revission realizada");
+                        System.out.println(vehiculoSeleccionado.mostrarInfo());
+
+                    } catch (NumberFormatException e) {
+                        System.out.println("tienes que ingreser solo numeros porfa");
+                    }
+
                     break;
                 case "3":
-                    
+                    revision = false;
                     break;
-            
+
                 default:
                     break;
             }
         }
     }
+    static void resumenVehiculo(){
+        int totalParticular = 0;
+        int totalCarga = 0;
+        long totalPrecio = 0;
+        for (VehiculoCarga vehiculoCarga : coleccion_VehiculoCarga) {
+           totalCarga++;
+           totalPrecio += (long) vehiculoCarga.calcularCostoRevision() * vehiculoCarga.getrevisionesPendientes(); 
+        }
+        for (VehiculoParticular vehiculoParticular : coleccion_VehivulosParticular) {
+            totalParticular ++;
+            totalPrecio += (long) vehiculoParticular.calcularCostoRevision() * vehiculoParticular.getrevisionesPendientes();
 
+        }
+        System.out.println("total de vehiculos particular :" + totalParticular);
+        System.out.println("total de vehiculo de carga : " + totalCarga);
+        System.out.println("facturacion total de todo los vehiculos : $" + totalPrecio);
 
+    }
 
 }
--- src/VehiculoParticular.java
+++ src/VehiculoParticular.java
@@ -1,10 +1,12 @@
 public class VehiculoParticular extends Vehiculo {
+    //esto seria el encapsulamiento donde los datos estan protegido y solo se pueden acceder por getter(mostrar dato) o setter(modificar datos)
     private int numPasajeros;
+    
     public VehiculoParticular(String patente, String marca, int revisionesPendientes, int numPasajeros){
         super(patente, marca, revisionesPendientes);
         this.numPasajeros = numPasajeros;
 
-    }
+    }// esta linea seria el metodo contructor que hereda los atributo de la clase padre que es Vehiculo ( se utilizar el comando super para eso)
     public int getnumPasajeros(){
         return this.numPasajeros;
     }
